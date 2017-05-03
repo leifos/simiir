26e1c4dc7bccf3fda05da92a90f789c1d5f1c88f
David Maxwell <maxwelld90@gmail.com>
2017-05-03 17:27:03 +0100

    Added satisfaction stopping strategy

---
+++ simiir/stopping_decision_makers/satisfaction_decision_maker.py
@@ -0,0 +1,34 @@
+from loggers import Actions
+from stopping_decision_makers.base_decision_maker import BaseDecisionMaker
+
+class SatisfactionDecisionMaker(BaseDecisionMaker):
+    """
+    A concrete implementation of a decision maker, implementing the satisfaction/count stopping rules.
+    Instructs a searcher to stop iif the searcher has found a predetermined number of snippets judged to be relevant.
+    """
+    def __init__(self, search_context, relevant_threshold=3):
+        """
+        Instantiates the decision maker, using a default relevant threshold of 3.
+        """
+        super(SatisfactionDecisionMaker, self).__init__(search_context)
+        self.__relevant_threshold = relevant_threshold  # The threshold; get to this point, and we abandon the SERP.
+    
+    def decide(self):
+        """
+        If the searcher has examined a given number of snippets judged to be relevant, then we abandon the search and issue another query.
+        Otherwise, we keep going until we find the required number of items (defined by self.__relevant_threshold).
+        """
+        counter = 0
+        examined_snippets = self._search_context.get_examined_snippets()[::-1]  # Reverse list
+        
+        for snippet in examined_snippets:
+            judgement = snippet.judgment
+            
+            if judgement > 0:
+                counter = counter + 1  # Found a relevant item; increment the counter.
+                
+                if counter == self.__relevant_threshold:  # If the counter is reached, abandon the SERP.
+                    return Actions.QUERY
+        
+        # If we get here, we need to keep looking.
+        return Actions.SNIPPET
