df4b6e8a2cea81e7ceb6878b56d6ef09cc6916db
David Maxwell <maxwelld90@gmail.com>
2017-05-12 12:36:46 +0100

    RBP decision maker disappeared; reimplemented it

--- simiir/stopping_decision_makers/rbp_decision_maker.py
+++ simiir/stopping_decision_makers/rbp_decision_maker.py
@@ -1,9 +1,11 @@
+import random
 from loggers import Actions
 from stopping_decision_makers.base_decision_maker import BaseDecisionMaker
 
 class RBPDecisionMaker(BaseDecisionMaker):
     """
-    A simple, stochastic-based 
+    An implementation of Rank-Biased Precision, operationalised as a stopping strategy. Uses a stochastic roll of the dice to determine
+    if a searcher continues or not. Implemented as per Moffat and Zobel (2008).
     """
     def __init__(self, search_context, logger, patience=0.5):
         """
@@ -18,4 +20,11 @@ class RBPDecisionMaker(BaseDecisionMaker):
         Implements the basic RBP algorithm, using the RBP score computed with the roll of a dice to determine whether
         the searcher continues or not.
         """
+        rank = self._search_context.get_current_serp_position()
+        rbp_score = self.__patience ** (rank - 1)
+        dp = random.random()
+        
+        if dp > rbp_score:
+            return Actions.QUERY
+        
         return Actions.SNIPPET
