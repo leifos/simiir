515d49c585ce3fe866a2fa9ea071a07e70b37591
David Maxwell <maxwelld90@gmail.com>
2017-05-13 12:05:31 +0100

    Added perfect TREC text classifier

---
+++ simiir/text_classifiers/perfect_classifier.py
@@ -0,0 +1,20 @@
+from ifind.seeker.trec_qrel_handler import TrecQrelHandler
+from simiir.text_classifiers.stochastic_informed_trec_classifier import StochasticInformedTrecTextClassifier
+
+class PerfectTrecTextClassifier(StochasticInformedTrecTextClassifier):
+    """
+    A simple text classifier that only judges items as relevant if they are actually TREC relevant.
+    """
+    def __init__(self, topic, search_context, qrel_file):
+        super(StochasticInformedTrecTextClassifier, self).__init__(topic, search_context, qrel_file)
+    
+    def is_relevant(self, document):
+        """
+        Returns true if the item is TREC relevant (where the judgement is >= 1); False otherwise.
+        """
+        val = self._get_judgment(self._topic.id, document.doc_id)
+        
+        if val > 0:
+            return True
+        
+        return False
