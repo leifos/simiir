513db4c3c77132dc24f94a9a8c40485910423b86
David Maxwell <maxwelld90@gmail.com>
2017-06-04 15:06:01 +0100

    Updated Whoosh interface for Redis caching

--- simiir/search_interfaces/whoosh_interface.py
+++ simiir/search_interfaces/whoosh_interface.py
@@ -1,6 +1,7 @@
 import os
 from whoosh.index import open_dir
 from simiir.search_interfaces import Document
+from ifind.search.cache import RedisConn
 from ifind.search.engines.whooshtrec import Whooshtrec
 from simiir.search_interfaces.base_interface import BaseSearchInterface
 import logging
@@ -16,13 +17,18 @@ class WhooshSearchInterface(BaseSearchInterface):
     Set model = 1 for BM25 (defaults to b=0.75), set pval to change b.
     Set model = 2 for PL2 (defaults to c=10.), set pval to change c.
     """
-    def __init__(self, whoosh_index_dir, model=2, implicit_or=True, pval=None, frag_type=2, frag_size=2, frag_surround=40):
+    def __init__(self, whoosh_index_dir, model=2, implicit_or=True, pval=None, frag_type=2, frag_size=2, frag_surround=40, host=None, port=0):
         super(WhooshSearchInterface, self).__init__()
         log.debug("Whoosh Index to open: {0}".format(whoosh_index_dir))
         self.__index = open_dir(whoosh_index_dir)
         self.__reader = self.__index.reader()
+        self.__redis_conn = None
         
-        self.__engine = Whooshtrec(whoosh_index_dir=whoosh_index_dir, model=model, implicit_or=implicit_or)
+        if host is not None:
+            self.__redis_conn = RedisConn(host=host, port=port)
+            self.__redis_conn.connect()
+        
+        self.__engine = Whooshtrec(whoosh_index_dir=whoosh_index_dir, model=model, implicit_or=implicit_or, cache='engine', host=host, port=port)
         
         # Update (2017-05-02) for snippet fragment tweaking.
         # SIGIR Study (2017) uses frag_type==1 (2 doesn't give sensible results), surround==40, snippet_sizes==2,0,1,4
