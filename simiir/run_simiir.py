4436d270b9acb1a5bad6fe4b2a74e000356a04b9
David Maxwell <maxwelld90@gmail.com>
2017-06-05 16:45:04 +0100

    Profiling

--- simiir/run_simiir.py
+++ simiir/run_simiir.py
@@ -23,7 +23,7 @@ def main(config_filename):
         progress = ProgressIndicator(configuration)
         #configuration.output.display_config()
         
-        print configuration
+        print dir(configuration)
         
         while not configuration.user.logger.is_finished():
             #progress.update()  # Update the progress indicator in the terminal.
