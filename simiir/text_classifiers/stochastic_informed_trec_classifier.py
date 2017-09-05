f5d9ef729c0017ecac548f76e98a6d266eb0911c
leifos <leifos@acm.org>
2017-09-05 20:07:58 +0100

    Fixed minor bug that resulted in slightly more clicks on non-relevant documents

--- simiir/text_classifiers/stochastic_informed_trec_classifier.py
+++ simiir/text_classifiers/stochastic_informed_trec_classifier.py
@@ -9,11 +9,11 @@ from ifind.seeker.trec_qrel_handler import TrecQrelHandler
 class StochasticInformedTrecTextClassifier(BaseInformedTrecTextClassifier):
     """
     Takes the TREC QREL file, and the probabilities rprob and nprob
-    if a document is TREC relevant, then the probability that it is judged relevant is given by rprob
-    if it is NOT TREC releavnt, then the probabiltiy that iit is judged non-relevant is given by nprob
+    if a document is TREC relevant, then the probability that it is clicked/judged relevant is given by rprob
+    if it is NOT TREC releavnt, then the probabiltiy that iit is clicked/judged non-relevant is given by nprob
 
     rprob and nprob are set to 1.0 by default, so that the classifier is deterministic,
-    i.e. it returns the exact TREC relevance judgement
+    i.e. it always clicks or judges relevant
     """
     def __init__(self, topic, search_context, qrel_file, rprob=1.0, nprob=1.0, base_seed=0, host=None, port=0):
         """
@@ -23,6 +23,7 @@ class StochasticInformedTrecTextClassifier(BaseInformedTrecTextClassifier):
         
         self._rel_prob = rprob
         self._nrel_prob = nprob
+
         
         self.__random = Random()
         self.__random.seed(base_seed + 256)
@@ -34,9 +35,25 @@ class StochasticInformedTrecTextClassifier(BaseInformedTrecTextClassifier):
         """
         val = self._get_judgment(self._topic.id, document.doc_id)
         dp = self.__random.random()
-        
+
+
+        #print(self._rel_prob, self._nrel_prob, dp, val)
+
+        threshold = self._nrel_prob
+        if val > 0:
+            threshold = self._rel_prob
+
+        if dp > threshold:
+            return False
+        else:
+            return True
+
+        """
+        # old code
         if val > 0: # if the judgement is relevant
+
             if dp > self._rel_prob:
+
                 return False
             else:
                 return True
@@ -45,3 +62,5 @@ class StochasticInformedTrecTextClassifier(BaseInformedTrecTextClassifier):
                 return True
             else:
                 return False
+
+        """
