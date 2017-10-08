12f180e4633d297b4bf6e20285b105638445dc0d
David Maxwell <maxwelld90@gmail.com>
2017-10-08 18:53:03 +0100

    Test

--- simiir/utils/data_handlers.py
+++ simiir/utils/data_handlers.py
@@ -101,7 +101,7 @@ class RedisDataHandler(FileDataHandler):
         
         if key_prefix is None:
             raise ValueError("A key prefix (string) must be specified for the RedisDataHandler.")
-        
+        print key
         key = '{key_prefix}::{hashed_key}'.format(key_prefix=key_prefix, hashed_key=hash(key))
         cache = redis.StrictRedis(host=host, port=port, db=0)
         
